24b0d5bf394199d9d77a1b8f9a9ae317f03928e1
DenKu <Denkuzia5155@gmail.com>
2024-10-14 21:49:44 +0300

    #7

--- src/main/java/com/example/seminar3exemple2REST/Service/RegistrationService.java
+++ src/main/java/com/example/seminar3exemple2REST/Service/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.example.seminar3exemple2REST.Service;
 
+import com.example.seminar3exemple2REST.Domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +11,19 @@ public class RegistrationService {
     private DataProcessingService dataProcessingService;
     private NotificationService notificationService;
 
-    public DataProcessingService getDataProcessingService() {
-        return dataProcessingService;
-    }
-
     public RegistrationService(UserService userService, DataProcessingService dataProcessingService, NotificationService notificationService) {
         this.userService = userService;
         this.dataProcessingService = dataProcessingService;
         this.notificationService = notificationService;
+    }
+
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    public void processRegistration(String name, int age, String email){
+        User createUser = userService.createUser(name, age, email);
+        getDataProcessingService().addUser(createUser);
+        notificationService.notifyUser(createUser);
     }
 }
